c2a118ab12cca2962b8040ed405928c7cc099096
LJS0000 <leejisu2281@gmail.com>
2025-04-05 02:36:19 +0900

    ✨ FEAT: 전역 QueryClient 설정 및 React Query Provider 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from 'next'
-import '@styles/globals.css'
+import type { Metadata } from 'next';
+import ReactQueryProvider from './provider';
+import '@styles/globals.css';
 
 export const metadata: Metadata = {
   title: '필기 PILGI',
   description: '필기 내용을 바로 클라우드에 업로드 해보세요',
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko'>
-      <body>{children}</body>
+    <html lang="ko">
+      <body>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
-  )
+  );
 }
