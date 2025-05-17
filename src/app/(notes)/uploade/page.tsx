f77ba223381d165c3f765e9ff944072f1d45219d
LJS0000 <leejisu2281@gmail.com>
2025-05-17 16:12:45 +0900

    ✨ FEAT: 업로드 페이지 생성

---
+++ src/app/(notes)/uploade/page.tsx
@@ -0,0 +1,13 @@
+import { Canvas } from '@components/feature';
+import { Toolbar } from '@components/feature';
+
+const EditorPage = () => {
+  return (
+    <div>
+      <Toolbar />
+      <Canvas />
+    </div>
+  );
+};
+
+export default EditorPage;
